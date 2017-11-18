4c464bf6a9120aa55ca842dcda9334d329ab8dec
joberton <joberton@academic.rrc.ca>
2017-11-18 07:05:04 +0000

    Validation has been applied to the new game page

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,19 +1,29 @@
 class GamesController < ApplicationController
 	def index
-		@games = params[:search] ?  Game.where("name LIKE ?", "%#{params[:search]}%").order(:name).page(params[:page]).per(3) : Game.all.page(params[:page]).per(3)
+		@games = params[:search] ?  Game.where("name LIKE ?", "%#{params[:search]}%").order(:name).page(params[:page]).per(8) : Game.all.page(params[:page]).per(8)
 	end
 
 	def new
-		@consoles = Console.all
-		@genres = Genre.all
+		setup
 	end
 
 	def create
+		setup
 		@game = Game.create(params.require(:game).permit(:name,:price,:date_released,:description,:console_id,:genre_id,:product_type_id,:image).merge(:product_type_id => 2))
-		redirect_to @game
+		if @game.valid? 
+			redirect_to @game
+		else
+			render 'new'
+		end
 	end
 
 	def show
 		@game = Game.find(params[:id])
+	end
+
+	def setup
+		@game = Game.new
+		@consoles = Console.all
+		@genres = Genre.all
 	end
 end
