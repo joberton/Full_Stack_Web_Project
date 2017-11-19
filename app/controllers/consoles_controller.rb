87ab6043252495a4971b449d4ffbde31ab063d7b
joberton <joberton@academic.rrc.ca>
2017-11-19 23:38:37 +0000

    The c of crud has been done for the consoles page

--- app/controllers/consoles_controller.rb
+++ app/controllers/consoles_controller.rb
@@ -9,8 +9,20 @@ class ConsolesController < ApplicationController
 		redirect_to shopping_cart_index_url
 	end
 
+	def new
+		@console = Console.new
+	end
+
+	def create
+		@console = Console.create(params.require(:console).permit(:name,:price,:date_released,:description,:image,:product_type_id).merge(:product_type_id => 1))
+		if(@console.valid?)
+			redirect_to @console
+		else
+			render 'new'
+		end
+	end
+
 	def show
 		@console = Console.find(params[:id])
 	end
-
 end
